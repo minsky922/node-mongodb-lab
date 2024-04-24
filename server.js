f8ebf0ac61c82decf2303ad900ee6293116bd95c
minsky922 <minsky922@gmail.com>
2024-04-24 19:32:11 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,144 @@
+const express = require('express')
+const app = express()
+const { MongoClient, ObjectId } = require('mongodb')
+
+const methodOverride = require('method-override')
+
+require('dotenv').config()
+app.use(methodOverride('_method'))
+
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+
+//요청.body 쓰려면 필요
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+let db;
+const url = process.env.DB_URL;
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결성공')
+    db = client.db('forum')
+    app.listen(process.env.PORT, () => {
+        console.log('http://localhost:8080 에서 서버 실행중')
+    })
+}).catch((err) => {
+    console.log(err)
+})
+
+function 미들웨어함수(요청, 응답, next) {
+
+    if (!요청.user) {
+        응답.send('로그인하세요')
+    }
+    //next = 미들웨어 코드실행 끝났으니 다음으로 이동해주세요
+    next()
+}
+
+// 미들웨어함수에 (요청,응답) 담아서 자동으로 먼저 실행됨
+app.get('/', 미들웨어함수, (요청, 응답) => {
+    응답.sendFile(__dirname + '/index.html')
+})
+
+app.get('/news', (요청, 응답) => {
+    db.collection('post').insertOne({ title: '어쩌' })
+    // 응답.send('오늘 비옴')
+})
+
+app.get('/list', async (요청, 응답) => {//모든 컬렉션 출력
+    let result = await db.collection('post').find().toArray()
+    console.log(result[0].title)
+    // 응답.send(result[0])
+
+    응답.render('list.ejs', { 글목록: result }) //서버데이터를 ejs 파일에 넣으려면
+})
+
+app.get('/write', (요청, 응답) => {
+    응답.render('write.ejs')
+})
+
+app.post('/add', async (요청, 응답) => {
+    console.log(요청.body)
+    //{ title: '제목', content: '내용' }
+
+    try {
+        if (요청.body.title == '') {
+            응답.send('제목입력안했는데?')
+        } else {
+            await db.collection('post').insertOne({
+                title: 요청.body.title,
+                content: 요청.body.content
+            })
+            응답.redirect('/list')
+        }
+    } catch (e) {
+        console.log(e)
+        응답.status(500).send('서버에러남')
+    }
+})
+
+// /detail/1 /detail/2 로 접속해도 밑 코드 실행
+app.get('/detail/:id', async (요청, 응답) => {
+
+    try {
+        let THISresult = await db.collection('post').findOne({
+            _id: new ObjectId(요청.params.id)
+        })
+        // console.log(요청.params)
+        if (THISresult == null) {
+            응답.status(400).send('이상한 url 입력함')
+        } else {
+            응답.render('detail.ejs', { resultPUT: THISresult })
+        }
+    } catch (e) {
+        console.log(e)
+        응답.status(404).send('이상한 url 입력함')
+    }
+})
+
+app.put('/edit', async (요청, 응답) => {
+    //     await db.collection('post').updateOne({ _id: 1 },
+    //         { $inc: { like: -2 } })
+
+    // })
+    await db.collection('post').updateOne({ _id: new ObjectId(요청.body.id) },
+        { $set: { title: 요청.body.title, content: 요청.body.content } })
+    //{ $set: { title: 새로운글제목, content: 새로운내용 } })
+    // let result = await db.collection('post').findOne({ _id: new ObjectId(요청.params.id) })
+    // 응답.render('edit.ejs', { result: result })
+    console.log(요청.body)
+    응답.redirect('/list')
+})
+
+// app.post('/abc', async (요청, 응답) => {
+//     console.log('인녕')
+//     console.log(요청.body)
+// })
+app.get('/abc', async (요청, 응답) => {
+    console.log(요청.query)
+})
+
+app.delete('/delete', async (요청, 응답) => {
+    // db에 있던 document 삭제하기~
+    // console.log(요청.query) 
+    // { docid: '66279ac228804bd05751332c' }
+    await db.collection('post').deleteOne({ _id: new ObjectId(요청.query.docid) })
+    응답.send('삭제완료')
+})
+
+app.get('/list/:id', async (요청, 응답) => {//모든 컬렉션 출력
+    let result = await db.collection('post').find()
+        .skip((요청.params.id - 1) * 5).limit(5).toArray()
+
+    응답.render('list.ejs', { 글목록: result }) //서버데이터를 ejs 파일에 넣으려면
+})
+
+//이게 더빠름 (단점 : 버튼 하나밖에 못만듬)
+// app.get('/list/next/:id', async (요청, 응답) => {//모든 컬렉션 출력
+//     let result = await db.collection('post')
+//         .find({ _id: { $gt: new ObjectId(요청.params.id) } })
+//         .limit(5).toArray()
+
+//     응답.render('list.ejs', { 글목록: result }) //서버데이터를 ejs 파일에 넣으려면
+// })
